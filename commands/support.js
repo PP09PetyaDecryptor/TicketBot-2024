cba20be924704bcc9e43191c627453a4bc7537c4
PP09PetyaDecryptor <166217226+PP09PetyaDecryptor@users.noreply.github.com>
2024-04-25 21:13:09 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -23,17 +23,17 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "support",
-  description: "Get support server link",
+  description: "Link do serwera",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
 
-     const supportServerLink = 'https://discord.gg/FUEHs7RCqz';
+     const supportServerLink = 'https://dsc.gg/roblox-i-gadanie-pl/';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle('Support server')
-            .setDescription(`Click [here](${supportServerLink}) to join our server.\nWe will be there for you anytime ❤️`)
+            .setDescription(`Zrobione przez PP09 [Serwer](${supportServerLink}).`)
             
             .setTimestamp();
 
